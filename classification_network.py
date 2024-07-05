63de898278bc24763c2cbc65fa97a320caf9b899
Graham Davies <grahamdaviesemail@gmail.com>
2024-07-05 18:12:28 +0100

    updated save predictions

--- classification_network.py
+++ classification_network.py
@@ -11,8 +11,10 @@ import sys
 import os
 import time
 import copy
+import pandas as pd
 
 from seasonal_data_split import balanced_winter, balanced_spring, balanced_summer, balanced_autumn
+
 
 class ScreenDataset(torch.utils.data.Dataset):
     def __init__(self, filenames, xmin=-1, xmax=-1, ymin=-1, ymax=-1):
@@ -26,29 +28,31 @@ class ScreenDataset(torch.utils.data.Dataset):
         self.ymin = ymin
         self.ymax = ymax
         self.filenames = filenames
-    
+
     def __len__(self):
         return len(self.filenames)
-    
+
     def __getitem__(self, item):
         img = Image.open(self.filenames[item])
         if self.xmin > 0 and self.xmax > 0 and self.ymin > 0 and self.ymax > 0:
             img = img.crop((self.xmin, self.ymin, self.xmax, self.ymax))
         return self.filenames[item], self.preprocess(img)
-        
+
+
 if __name__ == "__main__":
-    model_filepath = 'weights/winter_classifier.pth' # model filepath, eg: 'weights/classifier.pth'
-    image_filenames = balanced_winter['file_path'].tolist() # list of image filepaths, eg ["images/Cornwall_Crinnis/clear/2022_01_28_15_07.jpg", "images/Cornwall_Crinnis/blocked/2022_02_08_16_08.jpg"]
-    xmin = -1 # coordinates of the trash screen window (-1 if no window), eg 10
-    xmax = -1 # 235
-    ymin = -1 # 10
-    ymax = -1 # 235
-    threshold = 0.5 # blockage threshold value (between 0 and 1)
+    model_filepath = 'weights/winter_classifier.pth'  # model filepath, eg: 'weights/classifier.pth'
+    image_filenames = balanced_spring[
+        'file_path'].tolist()  # list of image filepaths, eg ["images/Cornwall_Crinnis/clear/2022_01_28_15_07.jpg", "images/Cornwall_Crinnis/blocked/2022_02_08_16_08.jpg"]
+    xmin = -1  # coordinates of the trash screen window (-1 if no window), eg 10
+    xmax = -1  # 235
+    ymin = -1  # 10
+    ymax = -1  # 235
+    threshold = 0.5  # blockage threshold value (between 0 and 1)
 
     batch_size = 32
-    
+
     device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-    
+
     dataset = ScreenDataset(image_filenames, xmin, xmax, ymin, ymax)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False)
     print(f"Using device: {device}")
@@ -56,18 +60,21 @@ if __name__ == "__main__":
     model = resnet50()
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, 2)
-    
+
     model.to(device)
     model.load_state_dict(torch.load(model_filepath, map_location=device))
     model.eval()
     softmax = nn.Softmax(dim=1)
 
+    # has a 'pred' column
+    balanced_spring['pred'] = None
+
     for filenames, images in dataloader:
         images = images.to(device)
         predictions = softmax(model(images)).detach()
         for i in range(len(filenames)):
-            balanced_winter.loc[balanced_winter['file_path'] == filenames[i], 'pred'] = "blocked" if predictions[
-                                                                                                         i, 1].item() > threshold else "clear"
+            prediction = "blocked" if predictions[i, 1].item() > threshold else "clear"
+            balanced_spring.loc[balanced_spring['file_path'] == filenames[i], 'pred'] = prediction
 
-        # Save balanced_winter dataframe with predictions
-        balanced_winter.to_csv('winter_predictions.csv', index=False)
+    # Save dataframe with predictions and original labels
+    balanced_spring.to_csv('spring_predictions.csv', index=False)
