ca98b190c20af0b83d292996ad8ef3004b5adac0
Graham Davies <grahamdaviesemail@gmail.com>
2024-07-30 09:28:26 +0200

    updated saliency mapping for multiple images + train_seasonal.py for complete data

--- train_seasonal.py
+++ train_seasonal.py
@@ -114,7 +114,7 @@ def train_model(model, dataloaders, criterion, optimizer, scheduler=None, num_ep
 
 
 # Choose the season to train on
-season_data = balanced_spring
+season_data = balanced_winter
 
 # Extract file paths and labels
 image_filenames = season_data['file_path'].tolist()
@@ -179,7 +179,7 @@ plt.show()
 save_model = input("Do you want to save the model? (yes/no): ").strip().lower()
 if save_model == 'yes':
     # Save the model
-    model_filepath = 'weights/spring_classifier.pth'  # Change name for saving
+    model_filepath = 'weights/winter_classifier.pth'  # Change name for saving
     torch.save(model.state_dict(), model_filepath)
     print(f"Model saved to {model_filepath}")
 else:
