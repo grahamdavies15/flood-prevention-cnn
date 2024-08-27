7ded6b7c45b225d5992051d537c9723a00755332
Graham Davies <grahamdaviesemail@gmail.com>
2024-08-27 17:20:00 +0100

    completed code

--- classification_network.py
+++ classification_network.py
@@ -4,8 +4,10 @@ from torchvision import transforms
 from torchvision.models import resnet50
 import torch.nn as nn
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-from seasonal_data_split import autumn_test, winter_test, spring_test
+from seasonal_data_split import winter_test, winter_test_labels, spring_test, spring_test_labels, autumn_test, autumn_test_labels
+
 
 class ScreenDataset(torch.utils.data.Dataset):
     def __init__(self, filenames, xmin=-1, xmax=-1, ymin=-1, ymax=-1):
@@ -30,16 +32,42 @@ class ScreenDataset(torch.utils.data.Dataset):
         return self.filenames[item], self.preprocess(img)
 
 
-def predict_for_dataset(season, model, device, test_filenames, threshold=0.5):
+def split_season_data(season_data, test_size=0.2, random_state=42):
+    image_filenames = season_data['file_path'].tolist()
+    labels = season_data['label'].apply(lambda x: 1 if x == 'blocked' else 0).tolist()
+    _, val_filenames, _, val_labels = train_test_split(image_filenames, labels, test_size=test_size,
+                                                       random_state=random_state)
+    return val_filenames, val_labels
+
+
+def predict_for_dataset(season, model, device, threshold=0.5):
+    # Use a dictionary to map the season name to the corresponding data variable
+    season_data_dict = {
+        'winter': winter_test,
+        'spring': spring_test,
+        'autumn': autumn_test,
+        'all': winter_test + spring_test + autumn_test
+    }
+
+    season_label_dict = {
+        'winter': winter_test_labels,
+        'spring': spring_test_labels,
+        'autumn': autumn_test_labels,
+        'all': winter_test_labels + spring_test_labels + autumn_test_labels
+    }
+
+    val_filenames = season_data_dict[season]
+    val_labels =  season_label_dict[season]
+
     xmin, xmax, ymin, ymax = -1, -1, -1, -1  # Adjust these as needed
 
-    screen_dataset = ScreenDataset(test_filenames, xmin, xmax, ymin, ymax)
+    screen_dataset = ScreenDataset(val_filenames, xmin, xmax, ymin, ymax)
     dataloader = torch.utils.data.DataLoader(screen_dataset, batch_size=64, shuffle=False)
 
     softmax = nn.Softmax(dim=1)
 
-    # Create a DataFrame to store predictions for the test set
-    val_df = pd.DataFrame({'file_path': test_filenames})
+    # Create a DataFrame to store predictions for the validation set
+    val_df = pd.DataFrame({'file_path': val_filenames, 'label': val_labels})
     val_df['pred'] = None
 
     for filenames, images in dataloader:
@@ -49,24 +77,17 @@ def predict_for_dataset(season, model, device, test_filenames, threshold=0.5):
             prediction = "blocked" if predictions[i, 1].item() > threshold else "clear"
             val_df.loc[val_df['file_path'] == filenames[i], 'pred'] = prediction
 
-    # Save the test set DataFrame with predictions
+    # Save the validation set DataFrame with predictions
     val_df.to_csv(f'csvs/{season}_pred_{model_season}_model.csv', index=False)
     print(f"Predictions saved for {season} test set.")
 
 
 if __name__ == "__main__":
-    # Import the test sets directly from script B
-    season_test_dict = {
-        'winter': winter_test,
-        'spring': spring_test,
-        'autumn': autumn_test,
-        'all': autumn_test + winter_test + spring_test  # Combine all test sets for 'all'
-    }
-
     for model_season in ["winter", "spring", "autumn", "all"]:  # Iterate over model seasons
         model_filepath = f'weights/{model_season}_classifier.pth'
 
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        device = torch.device("cuda" if torch.cuda.is_available() else
+                              ("mps" if torch.backends.mps.is_available() else "cpu"))
         print(f"Using device: {device}")
 
         # Load and prepare the model
@@ -79,5 +100,4 @@ if __name__ == "__main__":
 
         # Predict for each dataset (winter, spring, autumn, all)
         for season in ["winter", "spring", "autumn", "all"]:
-            test_filenames = season_test_dict[season]
-            predict_for_dataset(season, model, device, test_filenames)
+            predict_for_dataset(season, model, device)
